041ec041b7051c3dc72f8e6e2d78ca4755296379
Emiliano G. Scanga <scangaemiliano@gmail.com>
2022-08-03 14:43:22 +0200

    Create: data/db.js

---
+++ src/data/db.js
@@ -0,0 +1,18 @@
+export default[
+    {
+        first_name: "Emiliano",
+        last_name: "Scanga",
+        email: "scangaemiliano@gmail.com",
+        profile_picture: "",
+        sex: "uomo",
+        bio: "Appassionato nella creazione di Website e applicazioni Web da quando avevo 12 anni, creando i miei primi siti web con Microsoft Front Page, per poi passare direttamente al CMS WordPress.",
+        profession: "Junior Full Stack Web Developer",
+        work_place: "DevelHope",
+        skills: ["HTML", "CSS", "JavaScript", "React", "Agile", "Sass", "BoostStrap"],
+        open_to_work: true,
+        language: ["Inglese", "Italiano", "Spagnolo"],
+        javailable_for: "Lavoro in sede e remoto",
+        school: "Istituto Manuel Belgrano",
+        city: "Como",
+    }
+]
