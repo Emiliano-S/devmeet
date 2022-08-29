6e45b29982be366621f16fcf1e2a095af6875e99
[Leggio] <[leggio989898@gmail.com]>
2022-08-29 17:12:04 +0200

    Fix

--- src/data/db.js
+++ src/data/db.js
@@ -4,8 +4,7 @@ import MaryRose from '../assets/img/userPicture/MaryRose.png';
 
 export default[
     {
-        first_name: "Mario",
-        last_name: "Verde",
+        name: "Mario Verde",
         email: "marioverde@gmail.com",
         sex: "uomo",
         bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur. Blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat.", 
@@ -16,8 +15,7 @@ export default[
         user_picture: MarioVerde
     },
     {
-        first_name: "Mary",
-        last_name: "Rose",
+        name: "Mary Rose",
         email: "maryrose@gmail.com",
         sex: "donna",
         bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur. Blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat.", 
