be90c1ee6ea1d3996c5f909f3e49ece59a830fb0
[Leggio] <[leggio989898@gmail.com]>
2022-08-29 17:05:42 +0200

    Fix

--- src/data/db.js
+++ src/data/db.js
@@ -1,7 +1,6 @@
 export default[
     {
-        first_name: "Mario",
-        last_name: "Verde",
+        name: "Mario Verde",
         email: "marioverde@gmail.com",
         sex: "uomo",
         bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur. Blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat.", 
