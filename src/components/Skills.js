281c52860e510e28f04f2f0acd644e19355db245
Emiliano Scanga <scangaemiliano@gmail.com>
2022-09-14 11:14:47 +0200

    Aggiunto filtri skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,18 +1,82 @@
-const skillsArray = ['JavaScript', 'HTML', 'CSS', 'React', 'Node.js', 'Php', 'C', 'C#'];
-
+import { useRef } from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import skillsArray from '../data/dbSkills.json';
 
 export function Skills(){
+    const [selectedSkill, setSelectedSkills] = useState("");
+    const [dropdownSearchValue, setDropdownSearchValue] = useState("");
+    const [editMode, setEditMode] = useState(false);
+    const [mySkills, setMySkills] = useState([]);
+    const dropdownRef = useRef();
+
+    useEffect(()=>{
+        const checkIfClickedOutside = (target) =>{
+            if(editMode && dropdownRef.current && !dropdownRef.current.contains(target.target)){
+                setEditMode(false);
+            }
+        }
+        document.addEventListener('mousedown', checkIfClickedOutside);
+        return () =>{
+            document.removeEventListener('mousedown', checkIfClickedOutside);
+        }
+    }, [editMode]);
+
+    const skillSelectionHandler = (skill) =>{
+        setSelectedSkills(skill);
+        if(!mySkills.some(e => e.name === skill)){
+            setMySkills(prevSkill => [...prevSkill, {"name":skill,"age":1}]);
+        }
+        console.log(mySkills)
+        setDropdownSearchValue("");
+        setEditMode(false)
+    }
+
+    const filteredSkills = skillsArray.filter(skill => {
+        return skill.match(new RegExp(dropdownSearchValue, "i"));
+    })
 
     return(
         <div className="skillsPageContainer">
             <div className="skillsContainer">
                 <h3>Le tue skill</h3>
-                <input type="search" id='skillSearch' placeholder="Cerca Skills"/>
+                {editMode ? (<div ref={dropdownRef} className="dropdown-wrapper-search">
+                    <input
+                    type="search"
+                    name="dropdown-input"
+                    autoFocus
+                    onChange={e => setDropdownSearchValue(e.target.value)}
+                    value={dropdownSearchValue}
+                    />
+                    <div className="dropdown-list-search">
+                    <ul>
+                        {filteredSkills.map(skill => {
+                        return (
+                            <li key={skill} onClick={() => skillSelectionHandler(skill)}>
+                            {skill}{" "}
+                            </li>
+                        )
+                        })}
+                        {filteredSkills.length === 0 && (
+                        <li className="no-result">No results found</li>
+                        )}
+                    </ul>
+                    </div>
+                    </div>
+                    ): (
+                        <input
+                        type="search"
+                        id='skillSearch'
+                        placeholder="Cerca Skills"
+                        onFocus={() => setEditMode(true)}
+                        value={selectedSkill || "Cerca Skills"}
+                        />
+                    )}
                 <div class="skillsList">
-                    {skillsArray.map((element) => {
+                    {mySkills.map((element) => {
                         return(
                         <div class="skill">
-                            <span>{element}</span>
+                            <span>{element.name}</span>
                             <select class="selectYears">
                                 <option value='1'>1</option>
                                 <option value='2'>2</option>
