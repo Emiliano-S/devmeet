021845c3ace73750c96e9f8b5d1077ea78b7dbd6
Emiliano G. Scanga <scangaemiliano@gmail.com>
2022-09-24 16:00:22 +0200

    Finito feature

--- src/components/WorkExperiences.js
+++ src/components/WorkExperiences.js
@@ -4,13 +4,7 @@ import { WorkExperiencesForm } from "./WorkExperiencesForm";
 export function WorkExperiences() {
   return (
     <div className="skillsPageContainer">
-      <div className="skillsContainer">
-        <h3>Esperienze lavorative</h3>
-            <WorkExperiencesForm />
-      </div>
-      <div className="lavoroContainer">
-
-      </div>
+      <WorkExperiencesForm contentType="skill"/>
     </div>
   );
 }
--- src/components/WorkExperiencesForm.js
+++ src/components/WorkExperiencesForm.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import FormRow from "./FormRow";
 import Add from '../assets/svg/Add.svg'
+import { useRef } from "react";
 
 const workObject = {
     name: "",
@@ -9,9 +10,10 @@ const workObject = {
     dataFine: "",
 };
 
-export function WorkExperiencesForm(){
+export function WorkExperiencesForm({contentType}){
     const [work, setWork] = useState(workObject);
     const [worksExperience, setWorksExperience] = useState([]);
+    const elementRef = useRef([]);
 
     const handleChange = (e) =>{
         const {name, value} = e.target;
@@ -27,71 +29,97 @@ export function WorkExperiencesForm(){
         console.log(worksExperience);
     }
 
+    /*Funzione per rimuovere il valore selezionato dall'elenco*/
+    const handleValueRemove = (i) => {
+        elementRef.current[i].parentNode.className += " skillRemoved";
+        const remove = setTimeout(() => {
+            setWorksExperience((currentMyValue) =>
+                currentMyValue.filter((value, index) => index !== i)
+            );
+            clearTimeout(remove);
+        }, 600);
+    };
+
     return(
         <>
-            <form className="worksExperienceForm" onSubmit={handleSubmit}>
-                <FormRow
-                    textType
-                    text={'Nome azienda'}
-                    name={'name'}
-                    handleChange={handleChange}
-                    notRequired={true}
-                />
-                <FormRow
-                    textType
-                    text={'Qualifica'}
-                    name={'qualifica'}
-                    handleChange={handleChange}
-                />
-                <div className="container-df-row">
+           <div className="skillsContainer">
+                <h3>Esperienze lavorative</h3>
+                <form className="worksExperienceForm" onSubmit={handleSubmit}>
                     <FormRow
-                        data
-                        text={'Data Inizio'}
-                        name={'dataInizio'}
+                        textType
+                        text={'Nome azienda'}
+                        name={'name'}
                         handleChange={handleChange}
+                        notRequired={true}
                     />
                     <FormRow
-                        data
-                        text={'Data fine'}
-                        name={'dataFine'}
+                        textType
+                        text={'Qualifica'}
+                        name={'qualifica'}
                         handleChange={handleChange}
                     />
+                    <div className="container-df-row">
+                        <FormRow
+                            data
+                            text={'Data Inizio'}
+                            name={'dataInizio'}
+                            handleChange={handleChange}
+                        />
+                        <FormRow
+                            data
+                            text={'Data fine'}
+                            name={'dataFine'}
+                            handleChange={handleChange}
+                        />
+                    </div>
+                    <div style={{width: '100%', display: "flex", justifyContent: "center", paddingTop: '20px'}}>
+                        <button
+                        type="submit"
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            backgroundColor: 'rgb(54, 71, 100)',
+                            width: '266px',
+                            color: '#FFFFFF',
+                            fontWeight: "",
+                            borderRadius: "6px",
+                            height: '50px',
+                            border: "none",
+                            cursor: "pointer",
+                        }}
+                        >
+                            <div style={{width: '60%', display: 'flex', justifyContent:'flex-end'}}>
+                            AGGIUNGI
+                            </div>
+                            <div style= {{width: '40%', display: 'flex', justifyContent:'center'}}>
+                                <img src={Add} alt="+" />
+                            </div>
+                        </button>
+                    </div>
+                </form>
+            </div>
+            <div className="lavoroContainer">
+                <div className={`${contentType}List`}>
+                    {worksExperience.map((element, i) =>{
+                        return(
+                            <div className={`${contentType}Row`} key={element.name}>
+                                <div className={contentType} style={{flexDirection:'column'}} ref={(element) =>{
+                                    elementRef.current[i] = element;
+                                }}>
+                                <p>{element.name}</p>
+                                <p style={{fontSize:'12px'}}>{element.qualifica}</p>
+                                <p style={{fontSize:'12px'}}>dal {element.dataInizio} al {element.dataFine}</p>
+                                </div>
+                                <div
+                                className={`${contentType}Remove`}
+                                onClick={() => {
+                                    handleValueRemove(i);
+                                }}
+                                ></div>
+                            </div>
+                            );
+                    })}
                 </div>
-                <div style={{width: '100%', display: "flex", justifyContent: "center", paddingTop: '44px'}}>
-                    <button
-                    type="submit"
-                    style={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        backgroundColor: 'rgb(54, 71, 100)',
-                        width: '266px',
-                        color: '#FFFFFF',
-                        fontWeight: "",
-                        borderRadius: "6px",
-                        height: '50px',
-                        border: "none",
-                        cursor: "pointer",
-                    }}
-                    >
-                        <div style={{width: '60%', display: 'flex', justifyContent:'flex-end'}}>
-                        AGGIUNGI
-                        </div>
-                        <div style= {{width: '40%', display: 'flex', justifyContent:'center'}}>
-                            <img src={Add} alt="+" />
-                        </div>
-                    </button>
-                </div>
-            </form>
-            <div>
-                {worksExperience.map((work, i) =>{
-                    return(<div key={i}>
-                        <span>{work.name}</span>
-                        <br />
-                        <span>{work.qualifica}</span>
-                        <br />
-                        <span>{work.dataInizio} | {work.dataFine}</span>
-                    </div>)
-                })}
             </div>
         </>
     )
