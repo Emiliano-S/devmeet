d6a791fe5c58c6a5c938c557c61fd24d1ce2b390
Emiliano Scanga <scangaemiliano@gmail.com>
2022-09-27 15:15:45 +0200

    Fix uploadButton

--- src/components/UploadButton.js
+++ src/components/UploadButton.js
@@ -1,121 +1,187 @@
-import { useRef, useState } from "react"
+import { useRef, useState } from "react";
+import { set } from "react-hook-form";
 
-import Add from '../assets/svg/Add.svg'
-import Remove from '../assets/svg/Remove.svg'
+import Add from "../assets/svg/Add.svg";
+import Remove from "../assets/svg/Remove.svg";
 
 const UploadButton = ({
-    wd,
-    bgColor,
-    text,
-    textColor,
-    wg,
-    textClicked,
-    hg,
-    profile
-  }) =>  {
+  wd,
+  bgColor,
+  text,
+  textColor,
+  wg,
+  textClicked,
+  hg,
+  profile,
+}) => {
+  const [active, setActive] = useState(true);
+  const inputRef = useRef(null);
 
-const [active, setActive] = useState(true);
-const inputRef = useRef(null);
+  const [file, setFile] = useState("");
 
-const handleClick = () => {
-  inputRef.current.click();
-}
+  const handleClick = () => {
+    inputRef.current.click();
+  };
 
-const handleFileChange = (e) => {
-  const fileObj = e.target.files && e.target.files[0];
-  if (!fileObj){
-    return
-  }
-console.log(fileObj)
-  e.target.value = null;
+  const handleFileChange = (e) => {
+    // const fileObj = e.target.files && e.target.files[0];
+    // if (!fileObj) {
+    //   return;
+    // }
+    // console.log(fileObj);
+    // e.target.value = null;
 
-  console.log(fileObj)
-  console.log(fileObj.name)
-}
+    // console.log(fileObj);
+    // console.log(fileObj.name);
+    setFile(e.target.files[0]);
+  };
 
-const handleButtonClick = () => {
-setActive(false);
-handleClick();
+  const handleButtonClick = () => {
+    setFile("");
+    setActive(false);
+    handleClick();
+  };
 
+  const handleButtonClicked = () => {
+    setActive(true);
+  };
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        paddingTop: "44px",
+      }}
+    >
+      <input
+        style={{ display: "none" }}
+        ref={inputRef}
+        type="file"
+        onChange={handleFileChange}
+        accept=".pdf"
+        id="file"
+        name="file"
+      />
+      {active && (
+        <>
+          <button
+            onClick={handleButtonClick}
+            for="file"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              backgroundColor: bgColor,
+              width: "266px",
+              color: textColor,
+              fontWeight: wg,
+              borderRadius: "6px",
+              height: "50px",
+              border: "none",
+              cursor: "pointer",
+            }}
+          >
+            {!profile && (
+              <>
+                <div
+                  style={{
+                    width: "60%",
+                    display: "flex",
+                    justifyContent: "flex-end",
+                  }}
+                >
+                  {text}
+                </div>
+                {
+                  <div
+                    style={{
+                      width: "40%",
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <img src={Add} alt="+" />
+                  </div>
+                }{" "}
+              </>
+            )}
+
+            {profile && (
+              <div
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <div>{text}</div>
+              </div>
+            )}
+          </button>{" "}
+        </>
+      )}
+      {!active && (
+        <div>
+          {" "}
+          <button
+            onClick={handleButtonClicked}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              backgroundColor: bgColor,
+              width: "266px",
+              color: textColor,
+              fontWeight: wg,
+              borderRadius: "6px",
+              height: "50px",
+              border: "none",
+              cursor: "pointer",
+            }}
+          >
+            {!profile && (
+              <>
+                <div
+                  style={{
+                    width: "60%",
+                    display: "flex",
+                    justifyContent: "flex-end",
+                  }}
+                >
+                  {textClicked}
+                </div>
+                <div
+                  style={{
+                    width: "40%",
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                >
+                  <img src={Remove} alt="-" />
+                </div>{" "}
+              </>
+            )}
+            {profile && (
+              <div
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <div>{textClicked}</div>
+              </div>
+            )}
+          </button>
+          {!profile && (
+            <a href="#" style={{ fontSize: "12px", textDecoration: "none" }}>
+              {file.name}
+            </a>
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
-const handleButtonClicked = () => {
-setActive(true);
-}
-return (
-    <div style={{width: '100%', display: "flex", justifyContent: "center", paddingTop: '44px'}}>
-          {active && <>
-          <input 
-          style ={{display:"none"}}
-          ref= {inputRef}
-          type="file"
-          onChange={handleFileChange}
-          accept= ".pdf"
-          id="file"
-          name="file"
-          />
-          <button
-          onClick={handleButtonClick}
-          for= "file"
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            backgroundColor: bgColor,
-            width: '266px',
-            color: textColor,
-            fontWeight: wg,
-            borderRadius: "6px",
-            height: '50px',
-            border: "none",
-            cursor: "pointer",
-          }}
-        >
-            {!profile && <><div style={{width: '60%', display: 'flex', justifyContent:'flex-end'}}>
-          {text}
-            </div>
-            {<div style= {{width: '40%', display: 'flex', justifyContent:'center'}}>
-                <img src={Add} alt="+" />
-            </div>} </>}
-
-            {profile && <div style = {{width: '100%', display:"flex", justifyContent:"center"}}>
-              <div >
-                {text}
-              </div>
-              </div>}
-        </button> </>}
-        {!active && <div> <button
-          onClick={handleButtonClicked}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            backgroundColor: bgColor,
-            width: '266px',
-            color: textColor,
-            fontWeight: wg,
-            borderRadius: "6px",
-            height: '50px',
-            border: "none",
-            cursor: "pointer",
-          }}
-        >
-          {!profile && <><div style={{width: '60%', display: 'flex', justifyContent:'flex-end'}}>
-          {textClicked}
-            </div>
-            <div style= {{width: '40%', display: 'flex', justifyContent:'center'}}>
-                <img src={Remove} alt="-" />
-            </div> </>}
-            {profile && <div style = {{width: '100%', display:"flex", justifyContent:"center"}}>
-              <div >
-                {textClicked}
-              </div>
-              </div>}
-        </button>
-        {!profile && <a href="#" style= {{fontSize: "12px", textDecoration: "none"}}>Niente</a>}
-         </div>
-        }
-    </div>
-)
-}
-
-
-export default UploadButton
+export default UploadButton;
