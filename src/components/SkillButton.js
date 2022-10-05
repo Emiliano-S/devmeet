8e00a573c92804cf3d4df3b0078515bb0ef76a7b
Andrea Sabato <102291579+ndrSaba@users.noreply.github.com>
2022-10-05 13:44:17 +0200

    fix filterby skillbutton skillcomponent

--- src/components/SkillButton.js
+++ src/components/SkillButton.js
@@ -1,34 +1,48 @@
 import { useState } from "react";
 
-export function SkillButton({ id, title, Image, backgroundColor, gradient, boxheight, boxwidth, align }) {
-    const [active, setActive] = useState(false);
-    return (
-        <button className="ButtonSVG" key={id} 
-        onClick={() => {
-            setActive(!active);
-        }}
-            style={{
-                display: 'flex',
-                borderRadius: "10px",
-                border: "1px solid white",
-                borderWidth: "2px",
-                cursor: "pointer",
-                backgroundImage: `${gradient}`,
-                opacity: active ? "1" : "0.5",
-                borderColor: active ? "yellow" : "transparent",
-                width: `${boxwidth}`,
-                height: `${boxheight}`,
-                
-            }}>
-            <div className="SkillSVG">
-                <div className='svg'
-                style={{
-                    justifyContent: `${align}`,
-                }} >
-                    <Image />
-                </div>
-                <div className='TitleSVG'><div>{title}</div></div>
-            </div>
-        </button>
-    )
+export function SkillButton({
+  id,
+  title,
+  Image,
+  backgroundColor,
+  gradient,
+  boxheight,
+  boxwidth,
+  align,
+}) {
+  const [active, setActive] = useState(false);
+  return (
+    <button
+      className="ButtonSVG"
+      key={id}
+      onClick={() => {
+        setActive(!active);
+      }}
+      style={{
+        display: "flex",
+        border: "none",
+        borderRadius: "10px",
+        cursor: "pointer",
+        backgroundImage: `${gradient}`,
+        opacity: active ? "1" : "0.6",
+        boxShadow: active ? "0px 0px 12px 0px #545454" : "none",
+        width: `${boxwidth}`,
+        height: `${boxheight}`,
+      }}
+    >
+      <div className="SkillSVG">
+        <div
+          className="svg"
+          style={{
+            justifyContent: `${align}`,
+          }}
+        >
+          <Image />
+        </div>
+        <div className="TitleSVG">
+          <div>{title}</div>
+        </div>
+      </div>
+    </button>
+  );
 }
--- src/pages/Filterby.js
+++ src/pages/Filterby.js
@@ -1,15 +1,22 @@
 import { SkillComponent } from "../components/SkillComponent";
 
 export function Filterby() {
-    return (
-        <>
-            <div className="FilterBy">
-                <p>Filtra per:</p>
-            </div>
-            <div className="Container-Filter">
-
-                <SkillComponent />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div
+        className="FilterBy"
+        style={{ color: "#364764", fontWeight: "bold" }}
+      >
+        <p>Filtra per:</p>
+      </div>
+      <div
+        className="Container-Filter"
+        style={{
+          paddingBottom: "10%",
+        }}
+      >
+        <SkillComponent />
+      </div>
+    </>
+  );
 }
