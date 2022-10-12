484ef39243075070cfac70eb0b45ede3a20acb9d
Andrea Sabato <102291579+ndrSaba@users.noreply.github.com>
2022-10-12 18:19:38 +0200

    aggiunto PopUpRefuse per rifuta appuntamento, sistemato paragrafo PopUpReactivate e Deactivate

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -1,55 +1,175 @@
 import closer from "../assets/svg/xRemove.svg";
 import Button from "./Button";
 
-const PopUpAppointment = () =>{
-  return(
+const PopUpAppointment = () => {
+  return (
     <>
-    <span className="PopUpTitle">Fissa un appuntamento</span>
+      <span className="PopUpTitle">Fissa un appuntamento</span>
       <form>
         <div className="formAppointRow">
           <div className="formAppointInput">
             <label htmlFor="date">Data</label>
-            <input type="date" min={new Date().toISOString().split("T")[0]} className="input-form-style"/>
+            <input
+              type="date"
+              min={new Date().toISOString().split("T")[0]}
+              className="input-form-style"
+            />
           </div>
           <div className="formAppointInput">
             <label htmlFor="time">Ora</label>
-            <input type="time" className="input-form-style"/>
+            <input type="time" className="input-form-style" />
           </div>
         </div>
         <div className="formAppointRow">
           <div className="formAppointInput">
             <label htmlFor="time">Note</label>
-            <textarea type="time" className="input-form-style"/>
+            <textarea type="time" className="input-form-style" />
           </div>
         </div>
         <div className="formAppointRow">
           <Button
-                  submit
-                  type="submit"
-                  form="user-info"
-                  value="Submit"
-                  bgColor={"#fcf347"}
-                  wd={"100%"}
-                  hg={"3em"}
-                  text={"INVIA"}
-                  textColor={"rgb(54,71,100)"}
-                />
+            submit
+            type="submit"
+            form="user-info"
+            value="Submit"
+            bgColor={"#fcf347"}
+            wd={"100%"}
+            hg={"3em"}
+            text={"INVIA"}
+            textColor={"rgb(54,71,100)"}
+          />
         </div>
       </form>
     </>
-  )
-}
+  );
+};
 
-const PopUpDeleteAccount = () =>{
-  return(
+const PopUpDeleteAccount = () => {
+  return (
     <>
-    <span className="PopUpTitle">Elimina account</span>
-     <div className="PopUpText">
-      <p style={{fontFamily: 'DM Sans Bold'}}>Attenzione sei sicuro di voler eliminare il tuo account?</p>
-      <p> Puoi prenderti una pausa e puoi <strong>nascondere</strong> il tuo profilo momentaneamente. </p>
-     </div>
-     <div className="PopUpText">
+      <span className="PopUpTitle">Elimina account</span>
+      <div className="PopUpText">
+        <p style={{ fontFamily: "DM Sans Bold" }}>
+          Attenzione sei sicuro di voler eliminare il tuo account?
+        </p>
+        <p>
+          {" "}
+          Puoi prenderti una pausa e puoi <strong>nascondere</strong> il tuo
+          profilo momentaneamente.{" "}
+        </p>
+      </div>
+      <div className="PopUpText">
         <Button
+          submit
+          type="submit"
+          form="user-info"
+          value="Submit"
+          bgColor={"#fcf347"}
+          wd={"100%"}
+          hg={"3em"}
+          text={"ELIMINA ACCOUNT"}
+          textColor={"rgb(54,71,100)"}
+        />
+      </div>
+    </>
+  );
+};
+
+const PopUpDeactivateAccount = () => {
+  return (
+    <>
+      <span className="PopUpTitle">Nascondi profilo</span>
+      <div className="PopUpText">
+        <p style={{ fontFamily: "DM Sans Bold" }}>
+          Hai già trovato un lavoro e non vuoi più ricevere nuove proposte?
+        </p>
+        <p>
+          {" "}
+          Nascondi il tuo profilo e riattivalo in un secondo momento in modo
+          tale da non perdere i tuoi dati.{" "}
+        </p>
+      </div>
+      <div className="PopUpText">
+        <Button
+          submit
+          type="submit"
+          form="user-info"
+          value="Submit"
+          bgColor={"#fcf347"}
+          wd={"100%"}
+          hg={"3em"}
+          text={"NASCONDI PROFILO"}
+          textColor={"rgb(54,71,100)"}
+        />
+      </div>
+    </>
+  );
+};
+
+const PopUpReactivateAccount = () => {
+  return (
+    <>
+      <span className="PopUpTitle">Attiva profilo</span>
+      <div className="PopUpText">
+        <p style={{ fontFamily: "DM Sans Bold" }}>
+          Sei in cerca di lavoro? Riattiva il tuo profilo!
+        </p>
+        <p>
+          {" "}
+          Mostra nuovamente il tuo profilo per essere disponibile nella ricerca
+          del lavoro, potrai nasconderlo nuovamente se necessario.{" "}
+        </p>
+      </div>
+      <div className="PopUpText">
+        <Button
+          submit
+          type="submit"
+          form="user-info"
+          value="Submit"
+          bgColor={"#fcf347"}
+          wd={"100%"}
+          hg={"3em"}
+          text={"NASCONDI PROFILO"}
+          textColor={"rgb(54,71,100)"}
+        />
+      </div>
+    </>
+  );
+};
+
+const PopUpRefuse = () => {
+  return (
+    <>
+      <span className="PopUpTitle">Rifiuta appuntamento</span>
+      <div className="PopUpText">
+        <p style={{ fontFamily: "DM Sans Bold" }}>
+          Sei sicuro di voler rifiutare?
+        </p>
+        <p> Chiedi invece di riprogrammare l'appuntamento! </p>
+      </div>
+      <form>
+        <div className="formAppointRow">
+          <div className="formAppointInput">
+            <label htmlFor="date">Data</label>
+            <input
+              type="date"
+              min={new Date().toISOString().split("T")[0]}
+              className="input-form-style"
+            />
+          </div>
+          <div className="formAppointInput">
+            <label htmlFor="time">Ora</label>
+            <input type="time" className="input-form-style" />
+          </div>
+        </div>
+        <div className="formAppointRow">
+          <div className="formAppointInput">
+            <label htmlFor="time">Note</label>
+            <textarea type="time" className="input-form-style" />
+          </div>
+        </div>
+        <div className="formAppointRow">
+          <Button
             submit
             type="submit"
             form="user-info"
@@ -57,24 +177,10 @@ const PopUpDeleteAccount = () =>{
             bgColor={"#fcf347"}
             wd={"100%"}
             hg={"3em"}
-            text={"ELIMINA ACCOUNT"}
+            text={"INVIA"}
             textColor={"rgb(54,71,100)"}
           />
-     </div>
-    </>
-  )
-}
-
-const PopUpDeactivateAccount = () =>{
-  return(
-    <>
-    <span className="PopUpTitle">Nascondi profilo</span>
-     <div className="PopUpText">
-      <p style={{fontFamily: 'DM Sans Bold'}}>Hai già trovato un lavoro e non vuoi più ricevere nuove proposte?</p>
-      <p> Nascondi il tuo profilo e riattivalo in un secondo memento in modo tale da non perdere i tuoi dati. </p>
-     </div>
-     <div className="PopUpText">
-        <Button
+          <Button
             submit
             type="submit"
             form="user-info"
@@ -82,38 +188,14 @@ const PopUpDeactivateAccount = () =>{
             bgColor={"#fcf347"}
             wd={"100%"}
             hg={"3em"}
-            text={"ELIMINA ACCOUNT"}
+            text={"RIFIUTA"}
             textColor={"rgb(54,71,100)"}
           />
-     </div>
+        </div>
+      </form>
     </>
-  )
-}
-
-const PopUpReactivateAccount = () =>{
-  return(
-    <>
-    <span className="PopUpTitle">Mostra profilo</span>
-     <div className="PopUpText">
-      <p style={{fontFamily: 'DM Sans Bold'}}>Hai già trovato un lavoro e non vuoi più ricevere nuove proposte?</p>
-      <p> Nascondi il tuo profilo e riattivalo in un secondo memento in modo tale da non perdere i tuoi dati. </p>
-     </div>
-     <div className="PopUpText">
-        <Button
-            submit
-            type="submit"
-            form="user-info"
-            value="Submit"
-            bgColor={"#fcf347"}
-            wd={"100%"}
-            hg={"3em"}
-            text={"ELIMINA ACCOUNT"}
-            textColor={"rgb(54,71,100)"}
-          />
-     </div>
-    </>
-  )
-}
+  );
+};
 
 export function PopUp({ type, handleChange }) {
   return (
@@ -123,6 +205,7 @@ export function PopUp({ type, handleChange }) {
         {type === 2 && <PopUpDeleteAccount />}
         {type === 3 && <PopUpDeactivateAccount />}
         {type === 4 && <PopUpReactivateAccount />}
+        {type === 5 && <PopUpRefuse />}
         <div className="PopUpCloser" onClick={handleChange}>
           <img src={closer} alt="Chiudi PopUp" />
         </div>
--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -4,88 +4,149 @@ import { PopUp } from "../components/PopUp";
 import SettingsRow from "../components/SettingsRow";
 import UploadButton from "../components/UploadButton";
 
-
 const Settings = () => {
   const [popUpVisibility, setPopUpVisibility] = useState(false);
   const [popUpType, setPopUpType] = useState(0);
-  const [buttonTextVisibility, setButtontextVisibility] = useState('Nascondi profilo');
+  const [buttonTextVisibility, setButtontextVisibility] =
+    useState("NASCONDI PROFILO");
 
-  const closePopUp = (type) =>{
-    setPopUpType(type)
+  const closePopUp = (type) => {
+    setPopUpType(type);
     setPopUpVisibility(!popUpVisibility);
-  }
+  };
 
-  const changeText = () =>{
-      if(buttonTextVisibility === "Nascondi profilo"){
-        setButtontextVisibility("Mostra profilo");
-        closePopUp(3);
-      }else{
-        setButtontextVisibility("Nascondi profilo")
-        closePopUp(4);
-      }
-  }
+  const changeText = () => {
+    if (buttonTextVisibility === "NASCONDI PROFILO") {
+      setButtontextVisibility("ATTIVA PROFILO");
+      closePopUp(3);
+    } else {
+      setButtontextVisibility("ATTIVA PROFILO");
+      closePopUp(4);
+    }
+  };
 
   return (
     <>
-    {popUpVisibility  ?  <PopUp type={popUpType} handleChange={closePopUp} /> : ""}
+      {popUpVisibility ? (
+        <PopUp type={popUpType} handleChange={closePopUp} />
+      ) : (
+        ""
+      )}
       <div className="form-container">
         <div className="form-title-style">Notifiche</div>
         <div className="settings-container">
-          <Button selectButton name={"News"} bgColor={"white"} wd={"100"} hg={"40px"} text={"Email"} />
-          <Button selectButton name={"News"} bgColor={"white"} wd={"100"} hg={"40px"} text={"Notifiche Push"} />
-          <Button selectButton name={"News"} bgColor={"white"} wd={"100"} hg={"40px"} text={"SMS"} /> 
+          <Button
+            selectButton
+            name={"News"}
+            bgColor={"white"}
+            wd={"100"}
+            hg={"40px"}
+            text={"Email"}
+          />
+          <Button
+            selectButton
+            name={"News"}
+            bgColor={"white"}
+            wd={"100"}
+            hg={"40px"}
+            text={"Notifiche Push"}
+          />
+          <Button
+            selectButton
+            name={"News"}
+            bgColor={"white"}
+            wd={"100"}
+            hg={"40px"}
+            text={"SMS"}
+          />
         </div>
         <div className="form-title-style">Aiuto e assistenza</div>
-        <SettingsRow 
-        text = {"Aiuto e Assistenza"}
-        id =  "Help"
-        name = "Help"
-        link = "/"
+        <SettingsRow
+          text={"Aiuto e Assistenza"}
+          id="Help"
+          name="Help"
+          link="/"
         />
         <div className="form-title-style">Sicurezza</div>
-        <SettingsRow 
-        text = {"Linee Guida"}
-        id =  "Linee-Guida"
-        name = "Linee-Guida"
-        link = "/"
+        <SettingsRow
+          text={"Linee Guida"}
+          id="Linee-Guida"
+          name="Linee-Guida"
+          link="/"
         />
-        <SettingsRow 
-        text = {"Sicurezza e termini d'uso"}
-        id =  "Sicurezza"
-        name = "Sicurezza"
-        link = "/"
+        <SettingsRow
+          text={"Sicurezza e termini d'uso"}
+          id="Sicurezza"
+          name="Sicurezza"
+          link="/"
         />
-        <SettingsRow 
-        text = {"Consigli per la sicurezza"}
-        id =  "Consigli"
-        name = "Consigli"
-        link = "/"
+        <SettingsRow
+          text={"Consigli per la sicurezza"}
+          id="Consigli"
+          name="Consigli"
+          link="/"
         />
         <div className="form-title-style">Cookie Policy e Privacy</div>
-        <SettingsRow 
-        text = {"Impostazioni Privacy"}
-        id =  "Privacy"
-        name = "Privacy"
-        link = "/"
+        <SettingsRow
+          text={"Impostazioni Privacy"}
+          id="Privacy"
+          name="Privacy"
+          link="/"
         />
-        <SettingsRow 
-        text = {"Condizioni di utilizzo"}
-        id =  "Condizioni"
-        name = "Condizioni"
-        link = "/"
+        <SettingsRow
+          text={"Condizioni di utilizzo"}
+          id="Condizioni"
+          name="Condizioni"
+          link="/"
         />
-        <UploadButton 
+        {/* <UploadButton 
         profile
                           bgColor= {'#364764'}
                           text= {'NASCONDI PROFILO'}
                           textColor = {'white'}
                           textClicked ={'ATTIVA PROFILO'}
-                          wg = {'semibold'} />
-                          <div style={{width: "100%", display:"flex", justifyContent:"center", paddingTop:"20px"}}>
-      <button onClick={changeText}>{buttonTextVisibility}</button>
-      <Button submit text={'ELIMINA ACCOUNT'} textColor={'black'} bgColor={'yellow'} wd={'266px'} hg={'50px'} wg={'bold'} onClickFunction={()=>{closePopUp(2)}}/>
-                          </div>
-
+                          wg = {'semibold'} /> */}
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            paddingTop: "20px",
+          }}
+        >
+          <div
+            style={{ width: "100%", display: "flex", justifyContent: "center" }}
+          >
+            <button
+              style={{
+                backgroundColor: "#364764",
+                color: "white",
+                fontWeight: "bold",
+                borderRadius: "6px",
+                border: "none",
+                cursor: "pointer",
+                height: "50px",
+                width: "266px",
+              }}
+              onClick={changeText}
+            >
+              {buttonTextVisibility}
+            </button>
+          </div>
+          <Button
+            submit
+            text={"ELIMINA ACCOUNT"}
+            textColor={"black"}
+            bgColor={"yellow"}
+            wd={"266px"}
+            hg={"50px"}
+            wg={"bold"}
+            onClickFunction={() => {
+              closePopUp(3);
+            }}
+          />
+        </div>
       </div>
     </>
   );
