6b3a4648dd5aaa85a023f0924cfe97fcc8fb098d
Emiliano Scanga <scangaemiliano@gmail.com>
2022-10-13 10:30:57 +0200

    Aggiunti livello su skill e lingue

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,31 @@
+import { PopUp } from "./PopUp";
 import { motion } from "framer-motion";
 import { useState } from "react";
 import linkedinLogoIcon from "../assets/svg/linkedinLogoIcon.svg";
 import githubLogoIcon from "../assets/svg/githubIcon.svg";
 import websiteIcon from "../assets/svg/websiteIcon.svg";
 import cvIcon from "../assets/svg/cvIcon.svg";
-import { PopUp } from "./PopUp";
 
 export function Card({ user, id }) {
   const {
     first_name,
     last_name,
-    birthday,
+    birth_date,
     bio,
-    profession,
+    role,
     skills,
-    language,
-    available_for,
-    user_picture,
-    experiences,
+    city,
+    languages,
+    job_type,
+    profile_picture,
+    work_experience,
+    work_place,
     linkedin,
     github,
     website,
     cv,
   } = user;
   const [descriptionClass, setDescriptionClass] = useState("");
-
   const DescriptionOpener = (event, info) => {
     if (info.offset.y < -100) {
       setDescriptionClass("open");
@@ -43,7 +44,7 @@ export function Card({ user, id }) {
       <div className="CardRelative">
         <div
           className="CardProfilePicture"
-          style={{ backgroundImage: `url(${user_picture})` }}
+          style={{ backgroundImage: `url(${profile_picture})` }}
         ></div>
         <motion.div
           className={"CardProfileDescriptionContainer " + descriptionClass}
@@ -63,18 +64,19 @@ export function Card({ user, id }) {
             >
               <div className="CardDescriptionNameContainer">
                 <div className="CardName">
-                  {first_name} {last_name}, {GetAge(birthday)}
+                  {first_name} {last_name}, {GetAge(birth_date)}
                 </div>
-                <div className="AvailableForTag">{available_for}</div>
+                <div className="AvailableForTag">{job_type}</div>
               </div>
-              <div className="CardDescriptionProfessionContainer">
-                {profession}
-              </div>
+              <div className="CardDescriptionProfessionContainer">{role}</div>
               <div className="CardDescriptionSkillsContainer">
                 {skills.map((skill, index) => {
                   return (
                     <div key={index} className="SkillsCards">
-                      {skill}
+                      {skill.skill_name}
+                      <div className="SkillsLanguagesPop">
+                        {skill.skill_level}
+                      </div>
                     </div>
                   );
                 })}
@@ -86,10 +88,13 @@ export function Card({ user, id }) {
               <div className="CardDescriptionCamp">
                 <div className="CardDescriptionTitle">Lingue</div>
                 <div className="LanguageContainer">
-                  {language.map((lingua, index) => {
+                  {languages.map((lingua, index) => {
                     return (
                       <div key={index} className="LanguageCards">
-                        {lingua}
+                        {lingua.language}
+                        <div className="SkillsLanguagesPop">
+                          {lingua.language_level}
+                        </div>
                       </div>
                     );
                   })}
@@ -100,7 +105,7 @@ export function Card({ user, id }) {
                   Esperienze lavorative
                 </div>
                 <div className="ExperiencesContainer">
-                  {experiences.map((experiences, index) => {
+                  {work_experience.map((experiences, index) => {
                     return (
                       <div key={index} className="LanguageCards">
                         {experiences.society} {experiences.start_date} /{" "}
@@ -136,7 +141,7 @@ export function Card({ user, id }) {
                   {cv && (
                     <a href={cv}>
                       <img src={cvIcon} alt="My Curriculum Vitae!" />{" "}
-                      {first_name} {last_name} - {profession}.pdf
+                      {first_name} {last_name} - {role}.pdf
                     </a>
                   )}
                 </div>
@@ -146,18 +151,19 @@ export function Card({ user, id }) {
             <div className="descriptionContainer">
               <div className="CardDescriptionNameContainer">
                 <div className="CardName">
-                  {first_name} {last_name}, {GetAge(birthday)}
+                  {first_name} {last_name}, {GetAge(birth_date)}
                 </div>
-                <div className="AvailableForTag">{available_for}</div>
+                <div className="AvailableForTag">{job_type}</div>
               </div>
-              <div className="CardDescriptionProfessionContainer">
-                {profession}
-              </div>
+              <div className="CardDescriptionProfessionContainer">{role}</div>
               <div className="CardDescriptionSkillsContainer">
                 {skills.map((skill, index) => {
                   return (
                     <div key={index} className="SkillsCards">
-                      {skill}
+                      {skill.skill_name}
+                      <div className="SkillsLanguagesPop">
+                        {skill.skill_level}
+                      </div>
                     </div>
                   );
                 })}
@@ -169,10 +175,13 @@ export function Card({ user, id }) {
               <div className="CardDescriptionCamp">
                 <div className="CardDescriptionTitle">Lingue</div>
                 <div className="LanguageContainer">
-                  {language.map((lingua, index) => {
+                  {languages.map((lingua, index) => {
                     return (
                       <div key={index} className="LanguageCards">
-                        {lingua}
+                        {lingua.language}
+                        <div className="SkillsLanguagesPop">
+                          {lingua.language_level}
+                        </div>
                       </div>
                     );
                   })}
@@ -183,7 +192,7 @@ export function Card({ user, id }) {
                   Esperienze lavorative
                 </div>
                 <div className="ExperiencesContainer">
-                  {experiences.map((experiences, index) => {
+                  {work_experience.map((experiences, index) => {
                     return (
                       <div key={index} className="LanguageCards">
                         {experiences.society} {experiences.start_date} /{" "}
@@ -219,7 +228,7 @@ export function Card({ user, id }) {
                   {cv && (
                     <a href={cv}>
                       <img src={cvIcon} alt="My Curriculum Vitae!" />{" "}
-                      {first_name} {last_name} - {profession}.pdf
+                      {first_name} {last_name} - {role}.pdf
                     </a>
                   )}
                 </div>
--- src/pages/CardContainer.js
+++ src/pages/CardContainer.js
@@ -2,15 +2,18 @@ import { useState } from "react";
 import { Card } from "../components/Card";
 import { Loading } from "../components/Loading";
 import { PopUp } from "../components/PopUp";
+import dataUser from "../data/user.json";
 
 export function CardContainer({ data }) {
+  const dataUsers = JSON.parse(JSON.stringify(dataUser));
+
+  console.log(dataUsers.users);
 
   return (
     <div className="CardContainer">
-      {data.map((user, index) => {
+      {dataUsers.users.map((user, index) => {
         return <Card key={index} id={`card${index}`} user={user} />;
       })}
-      
     </div>
   );
 }
